e44a609d00bb4cf2832bff10beb6b5a8c5780773
RJC <rauljarodconanan@gmail.com>
2022-11-04 18:38:02 +0800

    >works most of the time >uses 6 bit primes >very inefficient >but it works(most of the time)

--- main.py
+++ main.py
@@ -16,28 +16,37 @@ def char_to_ascii(char):
     else:
         return ord(char)
 
-#poached from g4g
-def gcd(a, h):
-    temp = 0
-    while(1):
-        temp = a % h
-        if (temp == 0):
-            return h
-        a = h
-        h = temp
+def ascii_to_char(ascii):
+    """
+    Converts ascii characters to characters
+    """
+    if(type(ascii) is list):
+       return [chr(x) for x in ascii]
+    else:
+        return chr(ascii)
 
-def rsa_keygeneration_1024bit(message):
-    p = number.getPrime(512)
-    q = number.getPrime(512)
+#poached from g4g
+#def gcd(a, h):
+    #temp = 0
+    #while(1):
+        #temp = a % h
+        #if (temp == 0):
+            #return h
+        #a = h
+        #h = temp
+
+def rsa_keygeneration_6bit(message):
+    p = number.getPrime(6)
+    q = number.getPrime(6)
     n = p*q #public key
     e = 2 #public key
     CTF = math.lcm((p-1),(q-1))
 
     while (e < CTF):
-        if (gcd(e,CTF)==1):
+        if (math.gcd(e,CTF)==1):
             break
         else:
-            e += 1
+            e = e + 1
 
     # x*y == 1 (mod p)
     # d*e = 1(mod CTF)
@@ -48,22 +57,30 @@ def rsa_keygeneration_1024bit(message):
 
     in_ascii = char_to_ascii(to_convert)
     print("In ascii: ", in_ascii)
-
     #Encryption
     encrypted_data = []
     for x in in_ascii:
-        z = pow(x,e)
-        z = math.fmod(z,n)
-        encrypted_data.append(z)
-    print(encrypted_data)
+        c = pow(x,e,n)
+        encrypted_data.append(c)
+    print("Encrypted Data: ", encrypted_data)
+    encrypted_ascii = ascii_to_char(encrypted_data)
+    encrypted_ascii = ''.join(encrypted_ascii)
+    print("Encrypted Ascii: ", encrypted_ascii)
 
     #Decryption
     decrypted_data = []
+    for x in encrypted_data:
+        m = pow(x,d,n)
+        decrypted_data.append(m)
+    print("Decrypted Data: ", decrypted_data)
 
+    decrypted_ascii = ascii_to_char(decrypted_data)
+    decrypted_ascii = ''.join(decrypted_ascii)
+    print("Decrypted Message:" , decrypted_ascii)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     message = input()
-    rsa_keygeneration_1024bit(message)
+    rsa_keygeneration_6bit(message)
     #converted = char_to_ascii(message)
     #print(converted)
 
